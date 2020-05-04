1d3d1ff4a64fcc5b5d1ae67882f7e01654e57a1e
Chase <chase.olson@gmail.com>
2020-05-03 20:36:18 -0500

    Add pet detail items to pet screen

--- app/src/main/java/com/chaseolson/pets/details/PetDetailFragment.kt
+++ app/src/main/java/com/chaseolson/pets/details/PetDetailFragment.kt
@@ -1,17 +1,15 @@
 package com.chaseolson.pets.details
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.chaseolson.pets.R
-import com.chaseolson.pets.databinding.HomeScreenFragmentBinding
 import com.chaseolson.pets.databinding.PetDetailFragmentBinding
 import com.chaseolson.pets.main.MainActivityViewModel
 import com.google.android.material.appbar.AppBarLayout
